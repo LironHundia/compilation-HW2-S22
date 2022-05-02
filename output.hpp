dc83a83f30dfc521e18d586ff37a618b1eef66e5
LironHundia <73762306+LironHundia@users.noreply.github.com>
2022-05-02 08:04:21 +0300

    Add files via upload
    original files

---
+++ output.hpp
@@ -0,0 +1,16 @@
+
+<!-- saved from url=(0116)https://grades.cs.technion.ac.il/grades.cgi?cajfibgifdf72b63366ad202b8d52a+2+236360+Spring2022+hw/WCFiles/output.hpp -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=windows-1252"></head><body>#ifndef _236360_2_
+#define _236360_2_
+
+#include <string>
+
+namespace output {
+    extern const std::string rules[];
+    void printProductionRule(const int ruleno);
+    void errorLex(const int lineno);
+    void errorSyn(const int lineno);
+};
+
+#endif
+</string></body></html>
